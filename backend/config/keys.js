53ee72e2526f2b36d63a580d7f41bb72e6d569d3
novan ramadhan <novan.ramadhan@kompas.com>
2020-11-09 17:43:08 +0700

    update key

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,8 +1,8 @@
 module.exports={
-    mongoURI:'mongodb://localhost:27017/juraganpulsa',
-    jwtKeys:'novanganteng',
-    jwtKeysUser:'novangantenguser',
-    HOST_REDIS_LOCAL : "127.0.0.1",
-    HOST_REDIS_PORT : "6379",
-    urlLocal : '127.0.0.1:8000'
+    mongoURI:'',
+    jwtKeys:'',
+    jwtKeysUser:'',
+    HOST_REDIS_LOCAL : "",
+    HOST_REDIS_PORT : "",
+    urlLocal : ''
 };
